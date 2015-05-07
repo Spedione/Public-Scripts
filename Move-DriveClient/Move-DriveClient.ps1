720727829220f0733d3e87b481866c1cb58ddc10
Chris Hultin <chris.hultin@gmail.com>
2015-05-07 09:48:49 -0500

    Updated Move-DriveClient to Copy-Verify-Remove instead of simply moving files.  Added some error handling

--- Move-DriveClient/Move-DriveClient.ps1
+++ Move-DriveClient/Move-DriveClient.ps1
@@ -1,3 +1,5 @@
+#Version 0.1
+
 Param(
 	[Parameter(Mandatory=$True,Position=1)]
 	[string]$NewLocation
@@ -16,18 +18,33 @@ ELSE {
 While ( !( Test-Path $NewLocation )) {
 	$NewLocation = Read-Host 'New location? (If you enter "D:\", the new path will be "D:\Driveclient")'
 }
-$NewFullPath = $NewLocation + "Driveclient"
+$NewFullPath = $NewLocation + "Driveclient\"
 
 #Get DriveClient Services
 $DriveClient = Get-Service "DriveClient"
 $DriveClientUpdater = Get-Service "UpgradeRcbuSvc"
 
 #Stop DriveClient Services
-Stop-Service $DriveClient
-Stop-Service $DriveClientUpdater
+Stop-Service $DriveClient -ErrorAction Stop
+Stop-Service $DriveClientUpdater -ErrorAction Stop
 
 #Move DriveClient Folder
-Move-Item $CurrentLocation $NewLocation
+#Move-Item does not work when moving between drives.
+#Move-Item $CurrentLocation $NewLocation -ErrorAction Stop
+If (Test-Path $NewFullPath) {
+	Write-Host 'Destination already exists. Exiting'
+	Exit 1
+}
+Copy-Item $CurrentLocation $NewLocation -Recurse -ErrorAction Stop
+$CurrentLocationContents = Get-ChildItem -Recurse $CurrentLocation
+$NewLocationContents = Get-ChildItem -Recurse $NewFullPath
+$MissingFiles = Compare-Object -ReferenceObject $CurrentLocationContents -DifferenceObject $NewLocationContents
+If ( !($MissingFiles -eq $null)) {
+	'Source and Destination directories do not match. Exiting'
+	Exit 1
+}
+Remove-Item $CurrentLocation -Recurse
+
 
 #Update/Modify Registry Key
 Set-ItemProperty "HKLM:\SOFTWARE\Rackspace\CloudBackup" -Name "DataFolder" -Value $NewFullPath
